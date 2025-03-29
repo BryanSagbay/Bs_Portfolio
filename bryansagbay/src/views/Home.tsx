da626b67817f8533704dedd2eaa16ec1428746ad
Bryan Sagbay <bryansagbay2001@hotmail.com>
2025-03-29 16:01:30 -0500

    Se crea el proyecto inicial

---
+++ bryansagbay/src/views/Home.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex">
+      <div className="w-1/2 bg-gray-200 p-4">
+        <h2>Columna 1</h2>
+        <p>Contenido de la columna 1</p>
+      </div>
+      
+      <div className="w-1/2 bg-gray-100 p-4">
+        <h2>Columna 2</h2>
+        <p>Contenido de la columna 2</p>
+      </div>
+    </div>
+  );
+} 
