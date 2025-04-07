518b94649cbf7aae0c8b28368a66fc5a6376843e
Bryan Sagbay <bryansagbay2001@hotmail.com>
2025-04-07 10:26:24 -0500

    add new project for create API

--- API-porfolio/index.js
+++ API-porfolio/index.js
@@ -1,27 +1,23 @@
-import express from 'express';
-import {PORT} from './config.js';
+import express from 'express'
+import { PORT } from './config.js'
 
-const app = express();
+const app = express()
 
-app.get('/',(req, res)=>{
-    res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 }
 )
 
-//loging middleware
-app.post('/login', (req, res) => {
-    console.log('Log request received');
-    res.send('Log request received');
-}
+// loging
+app.post('/login', (req, res) => { }
 )
 
-//logout
+// logout
 app.post('/logout', (req, res) => {
-    console.log('Log request received');
-    res.send('Log request received');
 }
 )
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+}
+)
