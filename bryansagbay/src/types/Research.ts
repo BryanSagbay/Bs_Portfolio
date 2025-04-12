8b1eff51f001a7b012380a2892c6d241d3458334
Bryan Sagbay <bryansagbay2001@hotmail.com>
2025-04-11 20:05:30 -0500

    add new class ts

---
+++ bryansagbay/src/types/Research.ts
@@ -0,0 +1,11 @@
+
+export type ResearchArticle = {
+    id: string;
+    title: string;
+    description: string;
+    date: string;
+    timeread: string;
+    article: string;
+    comingsoon: boolean;
+  };
+  
