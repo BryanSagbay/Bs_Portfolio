916e41e7a931ee2f892cb8bf51e8d36e3a0614ad
Bryan Sagbay <bryansagbay2001@hotmail.com>
2025-04-18 10:34:06 -0500

    add new files

---
+++ Administrador/src/components/Research/Research.tsx
@@ -0,0 +1,8 @@
+export default function Research() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold mb-4">Investigación</h1>
+        <p className="text-lg text-gray-700">Aquí puedes realizar investigaciones sobre diversos temas.</p>
+        </div>
+    );
+    }
