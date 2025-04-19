7cc46795d563dbe12dee7f5a3daff307ad98b3d3
Bryan Sagbay <bryansagbay2001@hotmail.com>
2025-04-19 16:26:13 -0500

    add new settings

--- Administrador/vite.config.ts
+++ Administrador/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-});
-
+  base: '/',
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['admin-portfolio-ipdo.onrender.com'],
+  },
+})
