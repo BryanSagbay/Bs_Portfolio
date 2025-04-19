1c7cc43ef9d129901629182d51e31619dc111cbe
Bryan Sagbay <bryansagbay2001@hotmail.com>
2025-04-19 16:03:27 -0500

    git new config

--- Administrador/vite.config.ts
+++ Administrador/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['admin-portfolio-kz2q.onrender.com']
+  }
 })
